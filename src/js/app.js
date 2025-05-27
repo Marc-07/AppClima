4f47249c7280f36aab3c05ac5dac2596339427a7
Maria <maanraca01@gmail.com>
2025-05-27 12:25:21 -0500

    Se crea funcion para mostrar la temperaura actual en el html

--- src/js/app.js
+++ src/js/app.js
@@ -51,17 +51,41 @@ function consultarAPI(ciudad, pais){
 
     const appId = "d185bdc4da55853c53de4d7b11b9c151";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
-    console.log(url);
     fetch(url)
         .then( respuesta => {
             return respuesta.json();
         })
-        .then ( resultado => {
-            if(resultado.cod === "404"){
+        .then ( datosClima => {
+            limpiarHTML();
+            if( datosClima.cod === "404"){
                 mostrarError("Ciudad no encontrada");
+                return;
             }
+
+            //imprime los resultados o datos en el HTML
+            mostrarClima(datosClima);
         })    
+}
 
+function mostrarClima(datosClima) {
+    const { main: {temp, temp_max, temp_min} } = datosClima;
 
+    const centigrados = (parseInt(temp - 273.15));
 
+    const tempActual = document.createElement("P");
+    tempActual.innerHTML = `${centigrados} &#8451`;
+    tempActual.classList.add("font-bold", "text-6xl");
+
+    const resultadoDiv = document.createElement("DIV");
+    resultadoDiv.classList.add("text-center", "text-white", "py-10");
+    resultadoDiv.appendChild(tempActual);
+
+    resultado.appendChild(resultadoDiv);
+    
+}
+
+function limpiarHTML() {
+    while(resultado.firstChild){
+        resultado.removeChild(resultado.firstChild);
+    }
 }
