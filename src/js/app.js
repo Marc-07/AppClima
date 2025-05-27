23babdcc720866620c133e8988660caf8b90df36
Maria <maanraca01@gmail.com>
2025-05-27 09:47:20 -0500

    Se crea alerta para mostrar mensaje de error al usuario

--- src/js/app.js
+++ src/js/app.js
@@ -24,4 +24,14 @@ function buscarClima (e) {
 
 function mostrarError (mensaje){
     console.log(mensaje);
+
+    //Crear una alerta
+    const alerta = document.createElement("div");
+    alerta.classList.add("bg-red-600", "py-3", "text-center", "border-red-600", "rounded", "max-w-md", "mx-auto", "mt-6", "text-white")
+    alerta.innerHTML = `
+        <span class="block">${mensaje}</span>
+    `;
+
+    //Agregar al html
+    formulario.appendChild(alerta);
 }
