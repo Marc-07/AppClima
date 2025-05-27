a28755b1c6aad5a99e095ca5e3d6827d19000f12
Maria <maanraca01@gmail.com>
2025-05-27 10:27:25 -0500

    feat: Agrega consulta a la API de OpenWeather y muestra error si la ciudad no existe

--- src/js/app.js
+++ src/js/app.js
@@ -20,18 +20,48 @@ function buscarClima (e) {
 
         return;
     } 
+
+    //API
+    consultarAPI(ciudad, pais);
 }
 
 function mostrarError (mensaje){
-    console.log(mensaje);
+    const alerta = document.querySelector(".bg-red-600");
 
-    //Crear una alerta
-    const alerta = document.createElement("div");
-    alerta.classList.add("bg-red-600", "py-3", "text-center", "border-red-600", "rounded", "max-w-md", "mx-auto", "mt-6", "text-white")
-    alerta.innerHTML = `
-        <span class="block">${mensaje}</span>
-    `;
+    if(!alerta) {
+         //Crear una alerta
+        const alerta = document.createElement("div");
+        alerta.classList.add("bg-red-600", "py-3", "text-center", "border-red-600", "rounded", "max-w-md", "mx-auto", "mt-6", "text-white")
+        alerta.innerHTML = `
+            <span class="block">${mensaje}</span>
+        `;
 
-    //Agregar al html
-    formulario.appendChild(alerta);
+        //Agregar al html
+        formulario.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+
+    }
+
+}
+
+function consultarAPI(ciudad, pais){
+
+    const appId = "d185bdc4da55853c53de4d7b11b9c151";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${appId}`;
+    console.log(url);
+    fetch(url)
+        .then( respuesta => {
+            return respuesta.json();
+        })
+        .then ( resultado => {
+            if(resultado.cod === "404"){
+                mostrarError("Ciudad no encontrada");
+            }
+        })    
+
+
+
 }
